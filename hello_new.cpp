baed9e67500db9afc335fc36132985f5fdb6818b
unknown <sahanam308@gmail.com>
2023-02-15 21:47:48 +0530

    New cpp file

---
+++ hello_new.cpp
@@ -0,0 +1,11 @@
+//File: hello_new.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully created Jenkins pipeline\n";
+  cout << "SRN: PES1UG20CS365\n";
+  return 0;
+}
